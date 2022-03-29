60badc5b0411a4f309314abc626033cb01292126
Daniel Aliu <contact.danielaliu@gmail.com>
2022-03-29 18:29:40 +0100

    some github action changes

--- infrastructure/environments/main.tf
+++ infrastructure/environments/main.tf
@@ -91,7 +91,6 @@ resource "aws_ecs_service" "backend" {
   desired_count                      = 2
   deployment_minimum_healthy_percent = 50
   deployment_maximum_percent         = 100
-
   load_balancer {
     container_name   = "ceros-ski"
     container_port   = 80
