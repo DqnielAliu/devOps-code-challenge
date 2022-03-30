e8afbc9b4c4495bc313dc23e3c14d24614ba08b6
Daniel Aliu <contact.danielaliu@gmail.com>
2022-03-30 15:06:36 +0100

    seoarated iam from autoscaling.tf to iam.tf

---
+++ infrastructure/environments/iam.tf
@@ -0,0 +1,86 @@
+/**
+* The IAM policy needed by the ecs agent to allow it to manage the instances
+* that back the cluster.  This is the terraform structure that defines the
+* policy.
+*/
+data "aws_iam_policy_document" "ecs_agent" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:DescribeTags",
+      "ecs:CreateCluster",
+      "ecs:DeregisterContainerInstance",
+      "ecs:DiscoverPollEndpoint",
+      "ecs:Poll",
+      "ecs:RegisterContainerInstance",
+      "ecs:StartTelemetrySession",
+      "ecs:UpdateContainerInstancesState",
+      "ecs:Submit*",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
+
+/**
+* The policy resource itself.  Uses the policy document defined above.
+*/
+resource "aws_iam_policy" "ecs_agent" {
+  name        = "ceros-ski-ecs-agent-policy"
+  path        = "/"
+  description = "Access policy for the EC2 instances backing the ECS cluster."
+
+  policy = data.aws_iam_policy_document.ecs_agent.json
+}
+
+/**
+* A policy document defining the assume role policy for the IAM role below.
+* This is required.
+*/
+data "aws_iam_policy_document" "ecs_agent_assume_role_policy" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+
+}
+
+/**
+* The IAM role that will be used by the instances that back the ECS Cluster.
+*/
+resource "aws_iam_role" "ecs_agent" {
+  name = "ceros-ski-ecs-agent"
+  path = "/"
+
+  assume_role_policy = data.aws_iam_policy_document.ecs_agent_assume_role_policy.json
+}
+
+/**
+* Attatch the ecs_agent policy to the role.  The assume_role policy is attached
+* above in the role itself.
+*/
+resource "aws_iam_role_policy_attachment" "ecs_agent" {
+  role       = aws_iam_role.ecs_agent.name
+  policy_arn = aws_iam_policy.ecs_agent.arn
+}
+
+/**
+* The Instance Profile that associates the IAM resources we just finished
+* defining with the launch configuration.
+*/
+resource "aws_iam_instance_profile" "ecs_agent" {
+  name = "ceros-ski-ecs-agent"
+  role = aws_iam_role.ecs_agent.name
+}
